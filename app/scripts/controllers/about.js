24a6057abda8a4f4b43c5f09152172c4e95e1c36
Jeremy Melnyk <jeremyjm91@gmail.com>
2016-11-11 15:17:07 -0500

    Client scaffolding

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name travelsimpleApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the travelsimpleApp
+ */
+angular.module('travelsimpleApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
